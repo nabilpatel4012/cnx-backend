8c9a10c3554ac75cf524fc73ed60533a866ef067
nabilpatel4012 <nabilpatel4012@gmail.com>
2023-10-16 16:57:50 +0530

    fix order creation response

--- api/order.go
+++ api/order.go
@@ -6,15 +6,132 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/lib/pq"
 	db "github.com/nexpictora-pvt-ltd/cnx-backend/db/sqlc"
 )
 
 type createOrderRequest struct {
-	OrderID     int64         `json:"order_id" binding:"required"`
-	CustomerID  int           `json:"customer_id" binding:"required"`
-	ServiceIDs  pq.Int64Array `json:"service_ids" binding:"required"`
-	OrderStatus string        `json:"order_status" binding:"required"`
+	OrderID     int64   `json:"order_id" binding:"required"`
+	CustomerID  int     `json:"customer_id" binding:"required"`
+	ServiceIDs  []int32 `json:"service_ids" binding:"required"`
+	OrderStatus string  `json:"order_status" binding:"required"`
+}
+
+// func (server *Server) createOrder(ctx *gin.Context) {
+// 	var req createOrderRequest
+// 	if err := ctx.ShouldBindJSON(&req); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+
+// 	// Fix: Add a check to ensure that the service IDs array is not empty.
+// 	if len(req.ServiceIDs) == 0 {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "service_ids array cannot be empty"})
+// 		return
+// 	}
+// 	serviceIDs := pq.Int64Array(req.ServiceIDs)
+// 	var createdOrders []db.Order
+// 	// Iterate through the service IDs and create individual orders
+// 	for _, serviceID := range serviceIDs {
+
+// 		createdOrder, err := server.store.CreateOrder(ctx, db.CreateOrderParams{
+// 			OrderID:     int64(req.OrderID),
+// 			UserID:      int32(req.CustomerID),
+// 			ServiceIds:  int32(serviceID),
+// 			OrderStatus: req.OrderStatus,
+// 			// Set other fields as needed
+// 		})
+// 		if err != nil {
+// 			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 			return
+// 		}
+// 		createdOrders = append(createdOrders, createdOrder)
+// 	}
+// 	ctx.JSON(http.StatusOK, createdOrders)
+// }
+
+// func (server *Server) createOrder(ctx *gin.Context) {
+// 	var req createOrderRequest
+// 	if err := ctx.ShouldBindJSON(&req); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+// 		return
+// 	}
+
+// 	// Fix: Add a check to ensure that the service IDs array is not empty.
+// 	if len(req.ServiceIDs) == 0 {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "service_ids array cannot be empty"})
+// 		return
+// 	}
+
+// 	serviceIDs := pq.Int64Array(req.ServiceIDs)
+// 	var createdOrdersWithServices []struct {
+// 		ID                int64      `json:"id"`
+// 		OrderID           int64      `json:"order_id"`
+// 		UserID            int        `json:"user_id"`
+// 		OrderStatus       string     `json:"order_status"`
+// 		OrderStarted      time.Time  `json:"order_started"`
+// 		OrderDelivered    bool       `json:"order_delivered"`
+// 		OrderDeliveryTime time.Time  `json:"order_delivery_time"`
+// 		Services          db.Service `json:"services"`
+// 	}
+
+// 	// Iterate through the service IDs and create individual orders
+// 	for _, serviceID := range serviceIDs {
+// 		createdOrder, err := server.store.CreateOrder(ctx, db.CreateOrderParams{
+// 			OrderID:     int64(req.OrderID),
+// 			UserID:      int32(req.CustomerID),
+// 			ServiceIds:  int32(serviceID),
+// 			OrderStatus: req.OrderStatus,
+// 			// Set other fields as needed
+// 		})
+// 		if err != nil {
+// 			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 			return
+// 		}
+
+// 		// Fetch service details
+// 		service, err := server.store.GetService(ctx, int32(serviceID))
+// 		if err != nil {
+// 			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+// 			return
+// 		}
+
+// 		createdOrdersWithServices = append(createdOrdersWithServices, struct {
+// 			ID                int64      `json:"id"`
+// 			OrderID           int64      `json:"order_id"`
+// 			UserID            int        `json:"user_id"`
+// 			OrderStatus       string     `json:"order_status"`
+// 			OrderStarted      time.Time  `json:"order_started"`
+// 			OrderDelivered    bool       `json:"order_delivered"`
+// 			OrderDeliveryTime time.Time  `json:"order_delivery_time"`
+// 			Services          db.Service `json:"services"`
+// 		}{
+// 			ID:                int64(createdOrder.ID),
+// 			OrderID:           createdOrder.OrderID,
+// 			UserID:            int(createdOrder.UserID),
+// 			OrderStatus:       createdOrder.OrderStatus,
+// 			OrderStarted:      createdOrder.OrderStarted,
+// 			OrderDelivered:    createdOrder.OrderDelivered,
+// 			OrderDeliveryTime: createdOrder.OrderDeliveryTime,
+// 			Services:          service,
+// 		})
+// 	}
+
+// 	ctx.JSON(http.StatusOK, createdOrdersWithServices)
+// }
+
+type orderResponse struct {
+	OrderID           int64     `json:"order_id"`
+	UserID            int       `json:"user_id"`
+	OrderStatus       string    `json:"order_status"`
+	OrderStarted      time.Time `json:"order_started"`
+	OrderDelivered    bool      `json:"order_delivered"`
+	OrderDeliveryTime time.Time `json:"order_delivery_time"`
+	Services          []struct {
+		ServiceID    int64  `json:"service_id"`
+		ServiceName  string `json:"service_name"`
+		ServicePrice int    `json:"service_price"`
+		ServiceImage string `json:"service_image"`
+	} `json:"services"`
 }
 
 func (server *Server) createOrder(ctx *gin.Context) {
@@ -29,13 +146,19 @@ func (server *Server) createOrder(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "service_ids array cannot be empty"})
 		return
 	}
-	serviceIDs := pq.Int64Array(req.ServiceIDs)
+
+	var services []struct {
+		ServiceID    int64  `json:"service_id"`
+		ServiceName  string `json:"service_name"`
+		ServicePrice int    `json:"service_price"`
+		ServiceImage string `json:"service_image"`
+	}
 	var createdOrders []db.Order
-	// Iterate through the service IDs and create individual orders
-	for _, serviceID := range serviceIDs {
+	// Fetch service details for the response
+	for _, serviceID := range req.ServiceIDs {
 
 		createdOrder, err := server.store.CreateOrder(ctx, db.CreateOrderParams{
-			OrderID:     int64(req.OrderID),
+			OrderID:     req.OrderID,
 			UserID:      int32(req.CustomerID),
 			ServiceIds:  int32(serviceID),
 			OrderStatus: req.OrderStatus,
@@ -46,8 +169,51 @@ func (server *Server) createOrder(ctx *gin.Context) {
 			return
 		}
 		createdOrders = append(createdOrders, createdOrder)
+		// Fetch service details for the response
+		service, err := server.store.GetService(ctx, int32(serviceID))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+		}
+
+		services = append(services, struct {
+			ServiceID    int64  `json:"service_id"`
+			ServiceName  string `json:"service_name"`
+			ServicePrice int    `json:"service_price"`
+			ServiceImage string `json:"service_image"`
+		}{
+			ServiceID:    int64(service.ServiceID),
+			ServiceName:  service.ServiceName,
+			ServicePrice: int(service.ServicePrice),
+			ServiceImage: service.ServiceImage,
+		})
 	}
-	ctx.JSON(http.StatusOK, createdOrders)
+
+	// Construct the response object
+	response := struct {
+		OrderID           int64     `json:"order_id"`
+		UserID            int       `json:"user_id"`
+		OrderStatus       string    `json:"order_status"`
+		OrderStarted      time.Time `json:"order_started"`
+		OrderDelivered    bool      `json:"order_delivered"`
+		OrderDeliveryTime time.Time `json:"order_delivery_time"`
+		Services          []struct {
+			ServiceID    int64  `json:"service_id"`
+			ServiceName  string `json:"service_name"`
+			ServicePrice int    `json:"service_price"`
+			ServiceImage string `json:"service_image"`
+		} `json:"services"`
+	}{
+		OrderID:           req.OrderID,
+		UserID:            req.CustomerID,
+		OrderStatus:       req.OrderStatus,
+		OrderStarted:      createdOrders[0].OrderStarted, // Assuming you want the order_started time of the first created order
+		OrderDelivered:    createdOrders[0].OrderDelivered,
+		OrderDeliveryTime: createdOrders[0].OrderDeliveryTime,
+		Services:          services,
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
 
 type updateOrderStatusRequest struct {
